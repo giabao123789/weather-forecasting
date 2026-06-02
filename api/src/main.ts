6e267d0d46730c0ee29f0117593ac5ba8399cc1b
giabao123789 <giabao2461@gmail.com>
2026-06-02 18:53:32 +0700

    Allow all origins for CORS preflight support

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,10 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: ['https://weather-web-0m0x.onrender.com'],
+    origin: true,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
   app.useGlobalPipes(
     new ValidationPipe({
